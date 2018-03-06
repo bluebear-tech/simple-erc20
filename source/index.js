587d7f38f446b7f42d570d0c447a03b7c69aae7f
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-03-06 16:09:07 -0600

    Adding basic implementation

---
+++ source/index.js
@@ -0,0 +1,51 @@
+import Web3 from 'web3';
+import * as abi from './abi.json';
+
+const infuraEndpoints = {
+  1: "https://mainnet.infura.io",
+  'main': "https://mainnet.infura.io",
+  3: "https://ropsten.infura.io",
+  'ropsten': "https://ropsten.infura.io",
+  4: "https://rinkeby.infura.io",
+  'rinkeby': "https://rinkeby.infura.io",
+  42: "https://kovan.infura.io",
+  'kovan': "https://kovan.infura.io",
+}
+
+const initWeb3 = (network, web3Param) => {
+  if (typeof web3Param !== null) {
+    return new Web3(web3Param.currentProvider);
+  } else if (typeof web3 !== 'undefined') {
+    return new Web3(web3.currentProvider);
+  } else {
+    return new Web3(new Web3.providers.HttpProvider(infuraEndpoints[network]));
+  }
+};
+
+const SimpleERC20 = (address, network, web3Param = null) => {
+  const w3 = initWeb3(network, web3Param);
+  const contract = new w3.eth.Contract(abi, address);
+  const methods = contract.methods;
+
+  return {
+    contract,
+    name: () => methods.name().call(),
+    symbol: () => methods.symbol().call(),
+    totalSupply: () => methods.totalSupply().call(),
+    decimals: () => methods.decimals().call(),
+    balanceOf: (owner) => methods.balanceOf(owner).call(),
+    allowance: (owner, spender) => methods.allowance(owner, spender).call(),
+    name: () => methods.name().call()
+    //TODO: Transactions to possibly implement
+    //approve  (spender,value)
+    //transferFrom (from, to, value)
+    //transfer (to, value)
+
+    //TODO: Events to expose actions for.
+    // Approval
+    // Transfer
+  }
+
+};
+
+export default SimpleERC20;
