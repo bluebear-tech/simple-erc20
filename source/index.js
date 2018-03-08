90b3203839244521ddf65bdb879dd5c37447b013
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-03-07 22:19:37 -0600

    Adding the transactions if a coinbase is available.  This may need to be updated to be more syncronous

--- source/index.js
+++ source/index.js
@@ -48,7 +48,7 @@ const SimpleERC20 = (address, network = 1, web3Param = null) => {
     }
   });
 
-  return {
+  const erc20Obj = {
     contract,
     name: () => methods.name().call(),
     symbol: () => methods.symbol().call(),
@@ -56,16 +56,29 @@ const SimpleERC20 = (address, network = 1, web3Param = null) => {
     decimals: () => methods.decimals().call(),
     balanceOf: (owner) => methods.balanceOf(owner).call(),
     allowance: (owner, spender) => methods.allowance(owner, spender).call()
-    //TODO: Transactions to possibly implement
-    //approve  (spender,value)
-    //transferFrom (from, to, value)
-    //transfer (to, value)
+  };
 
-    //TODO: Events to expose actions for.
-    // Approval
-    // Transfer
-  }
+  w3.eth.getCoinbase().then((coinbase) => {
+    //TODO: review the timing possiblities with this.
+    erc20Obj.approve = async (spender, value) => {
+      const tx = methods.approve(spender, value);
+      tx.send({ from: coinbase, gas: await tx.estimateGas() })
+    };
+    erc20Obj.transferFrom = async (from, to, value) => {
+      const tx = methods.transferFrom(from, to, value);
+      tx.send({ from: coinbase, gas: await tx.estimateGas() })
+    };
+    erc20Obj.transfer = async (to, value) => {
+      const tx = methods.transfer(to, value);
+      tx.send({ from: coinbase, gas: await tx.estimateGas() })
+    };
+  }).catch(() => console.warn('SimpleERC20: web3 coinbase is undefined. Omitting transactions.'));
 
+  //TODO: Events to expose actions for.
+  // Approval
+  // Transfer
+
+  return erc20Obj;
 };
 
 module.exports = SimpleERC20;
